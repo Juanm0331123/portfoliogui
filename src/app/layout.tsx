fdce8a9d9dc65bcd4ee8b7c7afb9a16d846d0865
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2025-03-04 21:49:59 -0500

    Agregar estructura básica del portafolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import "../app/globals.css";
 import type { Metadata } from "next";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,19 @@ import Script from "next/script";
 
 export default function HomePage() {
   return (
-    <body>
+    <>
       <div id="particles-js"></div>
 
       <Header />
       <Hero />
 
-      <Script src="js/particles.min.js" strategy="lazyOnload" />
-      <Script src="js/particle.js" strategy="lazyOnload" />
-
-      <Script src="js/script.js" />
-
-      <Script src="https://unpkg.com/swiper/swiper-bundle.min.js" />
-    </body>
+      <Script src="/js/particles.min.js" strategy="lazyOnload" />
+      <Script src="/js/particle.js" strategy="lazyOnload" />
+      <Script src="/js/script.js" strategy="lazyOnload" />
+      <Script
+        src="https://unpkg.com/swiper/swiper-bundle.min.js"
+        strategy="beforeInteractive"
+      />
+    </>
   );
 }
